9ada834fc2a67ad775337671c690400563e474bd
MHarton <marcusharton18@gmail.com>
2018-04-24 01:52:10 -0400

    Add files via upload

---
+++ Weather.c
@@ -0,0 +1,47 @@
+#include "Weather.h"
+
+
+
+void HAL_UART_RxCpltCallback (UART_HandleTypeDef * huart)
+{
+	if(!strcmp(pdata, "\r") || !strcmp(pdata, "\n"))
+	{
+		pos = 0;
+		buff_flag = 1;
+	}
+	else
+	{
+		buffer[pos++] = pdata[0];
+	}
+	HAL_UART_Receive_IT(&huart1, pdata, 1);
+}
+
+
+int initialize_WIFI()
+{
+		HAL_UART_Transmit(&huart1, choosemod, strlen(choosemod), 1000);
+	    HAL_UART_Receive_IT(&huart1, pdata, 1);
+	  	if(strncmp(pdata, "OK", 2))
+        {       
+           return 0; 
+        }
+		HAL_UART_Transmit(&huart1, connect, strlen(connect), 1000);
+	    HAL_UART_Receive_IT(&huart1, pdata, 1);
+	  	if(strncmp(pdata, "OK", 2))
+        {       
+           return 0; 
+        }
+		HAL_UART_Transmit(&huart1, enable_multcha, strlen(enable_multcha), 1000);
+	    HAL_UART_Receive_IT(&huart1, pdata, 1);
+	  	if(strncmp(pdata, "OK", 2))
+        {       
+           return 0; 
+        }
+		HAL_UART_Transmit(&huart1, datamode, strlen(datamode), 1000);
+	    HAL_UART_Receive_IT(&huart1, pdata, 1);
+	  	if(strncmp(pdata, "OK", 2))
+        {       
+           return 0; 
+        }
+        return 1;
+}
---
+++ Weather.h
@@ -0,0 +1,30 @@
+#ifndef WEATHER
+#define WEATHER
+#include "stm32f0xx_hal.h"
+#include "stdlib.h"
+
+
+
+
+
+//Initialization
+extern const char status[] = "AT\r\n";
+extern const char choosemod[]= "AT+CWMODE=1\r\n";
+extern const char connect[] = "AT+CWJAP=\"NetworkName\",\"password\"\r\n";
+
+//Further initialiation
+extern const char enable_multcha[] = "AT+CIPMUX=1\r\n";//Enable multiple channel connections 
+extern const char datamode[] = "AT+CIPMODE=1\r\n";
+
+
+//General
+extern const char weather_pull[] = "api.openweathermap.org/data/2.5/find?q=\"West Lafayette\"&units=imperial"
+
+
+
+void HAL_UART_RxCpltCallback (UART_HandleTypeDef * huart);
+int initialize_WIFI();
+
+
+
+#endif
