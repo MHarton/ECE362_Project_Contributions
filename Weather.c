beeb378dc80113b5af5b09bf6c67ff99b06498e0
MHarton <marcusharton18@gmail.com>
2018-04-27 00:26:35 -0400

    Add files via upload

--- Weather.c
+++ Weather.c
@@ -1,47 +1,119 @@
 #include "Weather.h"
 
 
-
-void HAL_UART_RxCpltCallback (UART_HandleTypeDef * huart)
+int control_unit()
 {
-	if(!strcmp(pdata, "\r") || !strcmp(pdata, "\n"))
-	{
-		pos = 0;
-		buff_flag = 1;
-	}
-	else
-	{
-		buffer[pos++] = pdata[0];
-	}
-	HAL_UART_Receive_IT(&huart1, pdata, 1);
-}
 
+  pos = 0;
+  //Weather and JSON setup
+  curr_weather = (wdata *)malloc(sizeof(wdata));
+  sprintf(set_msg_size, "AT+CIPSEND=%d\r\n", strlen(weather_pull_test_2));
+  initialize_WIFI();
+  grab_Weather();
+  test_Weather_UART();
+ // HAL_Delay(5000);
+  close_and_check();
+  free_everything();
+  return 1;
+}
 
 int initialize_WIFI()
 {
-		HAL_UART_Transmit(&huart1, choosemod, strlen(choosemod), 1000);
-	    HAL_UART_Receive_IT(&huart1, pdata, 1);
-	  	if(!strncmp(pdata, "ERROR", 2))
-        {       
-           return 0; 
-        }
-		HAL_UART_Transmit(&huart1, connect, strlen(connect), 1000);
-	    HAL_UART_Receive_IT(&huart1, pdata, 1);
-	  	if(!strncmp(pdata, "ERROR", 2))
-        {       
-           return 0; 
-        }
-		HAL_UART_Transmit(&huart1, enable_multcha, strlen(enable_multcha), 1000);
-	    HAL_UART_Receive_IT(&huart1, pdata, 1);
-	  	if(!strncmp(pdata, "OK", 2))
-        {       
-           return 0; 
-        }
-		HAL_UART_Transmit(&huart1, datamode, strlen(datamode), 1000);
-	    HAL_UART_Receive_IT(&huart1, pdata, 1);
-	  	if(!strncmp(pdata, "OK", 2))
-        {       
-           return 0; 
-        }
-        return 1;
+	  HAL_UART_Receive_IT(&huart1, pdata, 1);
+	  HAL_UART_Transmit(&huart1, rset, strlen(rset), 800);
+	  HAL_UART_Receive_IT(&huart1, pdata, 1);
+	  HAL_Delay(500);
+	  while(strncmp(buffer, "ready", 5))
+	  {
+	     buff_flag = 1;
+	  }
+	  HAL_UART_Transmit(&huart1, connect, strlen(connect), 800);
+	  HAL_UART_Receive_IT(&huart1, pdata, 1);
+	  HAL_Delay(500);
+	  while(strncmp(buffer, "OK", 2))
+	  {
+	     buff_flag = 1;
+	  }
+	  return 1;
 }
+int grab_Weather()
+{
+	  HAL_UART_Transmit(&huart1, start_sock_weather, strlen(start_sock_weather), 800);
+	  HAL_UART_Receive_IT(&huart1, pdata, 1);
+	  HAL_Delay(500);
+	  while(strncmp(buffer, "OK", 2))
+	  {
+	     buff_flag = 1;
+	  }
+	  HAL_UART_Transmit(&huart1, set_msg_size, strlen(set_msg_size), 800);
+	  HAL_UART_Receive_IT(&huart1, pdata, 1);
+	  HAL_Delay(500);
+	  while(strncmp(buffer, ">", 1))
+	  {
+	     buff_flag = 1;
+	  }
+	  HAL_UART_Transmit(&huart1, weather_pull_test_2, strlen(weather_pull_test_2), 800);
+	  HAL_UART_Receive_IT(&huart1, pdata, 1);
+	  HAL_Delay(1000);
+	  while(strncmp(buffer, "{\"coord\"", 7))
+	  {
+	     buff_flag = 1;
+	  }
+	  jstr = (char *)malloc(sizeof(char)*strlen(buffer));
+	  strncpy(jstr, buffer, strlen(buffer));
+	  jTabby = split_a_string(buffer);
+	  jsonParse(*jTabby, curr_weather);
+	  HAL_Delay(1000);
+
+	  return 1;
+}
+
+
+int test_Weather_UART()
+{
+	  HAL_UART_Transmit(&huart1, curr_weather->weather, strlen(curr_weather->weather), 800);
+	  HAL_UART_Receive_IT(&huart1, pdata, 1);
+	  while(strncmp(buffer, "ERROR", 5))
+	  {
+	     buff_flag = 1;
+	  }
+	  HAL_UART_Transmit(&huart1, curr_weather->temp, strlen(curr_weather->temp), 800);
+	  HAL_UART_Receive_IT(&huart1, pdata, 1);
+	  while(strncmp(buffer, "ERROR", 5))
+	  {
+	     buff_flag = 1;
+	  }
+	  return 1;
+}
+
+int free_everything()
+{
+	  free_jTable(jTabby);
+	  free(jstr);
+	  return 1;
+}
+
+int free_weather()
+{
+	  clean_Weather(curr_weather);
+	  free_Weather(curr_weather);
+    return 1;
+}
+
+int close_and_check()
+{
+	  HAL_UART_Transmit(&huart1, close_socket, strlen(close_socket), 1000);
+	  HAL_UART_Receive_IT(&huart1, pdata, 1);
+	  HAL_Delay(100);
+	  while(strncmp(buffer, "OK", 2)){}
+	  HAL_UART_Transmit(&huart1, status, strlen(status), 800);
+	  HAL_UART_Receive_IT(&huart1, pdata, 1);
+	  HAL_Delay(500);
+	  if(!strncmp(buffer, "OK", 2) || !strncmp(buffer, "AT", 2))
+	  {
+	     return 1;
+	  }
+	  return 0;
+
+}
+
--- Weather.h
+++ Weather.h
@@ -2,30 +2,57 @@
 #define WEATHER
 #include "stm32f0xx_hal.h"
 #include "stdlib.h"
+#include "jsonParser.h"
+//Characters and so forth
 
+extern unsigned char status[0];
+extern unsigned char echoff[80]; 
+extern unsigned char choosemod[80];
+extern unsigned char connect[80];
+extern unsigned char rset[8];  
+extern unsigned char start_sock_test[80];
+extern unsigned char start_sock_weather[80]; 
 
-
-
-
-//Initialization
-extern const char status[] = "AT\r\n";
-extern const char echoff[] = "ATE0\r\n";
-extern const char choosemod[]= "AT+CWMODE=1\r\n";
-extern const char connect[] = "AT+CWJAP=\"NetworkName\",\"password\"\r\n";
+extern unsigned char send_msg[80];  
+extern unsigned char set_msg_size[80];
+extern unsigned char close_socket[80];= 
 
 //Further initialiation
-extern const char enable_multcha[] = "AT+CIPMUX=1\r\n";//Enable multiple channel connections 
-extern const char datamode[] = "AT+CIPMODE=1\r\n";
+extern unsigned char datamode[80]; = 
+extern unsigned char enable_multcha[80]; 
 
 
 //General
-extern const char weather_pull[] = "api.openweathermap.org/data/2.5/find?q=\"West Lafayette\"&units=imperial"
+extern unsigned char weather_pull_test[150]; 
+extern unsigned char weather_pull_test_2[150];
+extern unsigned char weather_pull[80]; 
+extern unsigned char list_wifi[80]; 
 
 
+extern char *raw_weather;
 
-void HAL_UART_RxCpltCallback (UART_HandleTypeDef * huart);
+//Utility
+extern char pdata[20];
+extern uint8_t tdata;
+extern int pos;
+extern char buffer[500];
+//flags
+extern int buff_flag;
+
+//
+extern wdata *curr_weather;
+extern jtable *jTabby;
+extern char *jstr;
+
+
+//Functions
+int control_unit();
 int initialize_WIFI();
-
+int grab_Weather();
+int test_Weather_UART();
+int free_everything();
+int free_weather();
+int close_and_check();
 
 
 #endif
